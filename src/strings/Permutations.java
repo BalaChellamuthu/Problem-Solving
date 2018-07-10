f0e342b7f5eea248456b663491a1d049716b62e7
Balasubramaniyam Chellamuthu <balasubramaniyam_chellamuthu@choicehotels.com>
2018-07-09 23:06:15 -0700

    Added javadoc

--- src/strings/Permutations.java
+++ src/strings/Permutations.java
@@ -1,5 +1,7 @@
 package strings;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -7,10 +9,19 @@ public class Permutations {
 
 	static Set<String> uniqueCombinations = new HashSet<String>();
 
+	/**
+	 * Move the first character to last in the string to form a new string and then check if the new string is not in the set.
+	 * If it is not in the Set then add it to the Set.
+	 * Repeat the process for newly formed string until newly formed string is not exists in the Set.
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		String input = "Bala";
+		LocalDateTime startTime = LocalDateTime.now();
+		String input = "Test";
 		permutation(input);
 		System.out.println(uniqueCombinations);
+		LocalDateTime endTime = LocalDateTime.now();
+		System.out.println("Difference in seconds " + Duration.between(startTime, endTime).getSeconds());
 	}
 
 	private static void permutation(String input) {
